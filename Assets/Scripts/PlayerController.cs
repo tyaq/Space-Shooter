bdb04c34af364be9cb964678d7601847ad2902a5
Ishtyaq Habib <ishtyaq.habib@gmail.com>
2015-11-15 21:39:59 -0500

    Added Ability to Shoot with Fire1
    Press mouseLeft to shoot a bolt.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,12 +11,23 @@ public class PlayerController : MonoBehaviour {
 	public float speed;
 	public float tilt;
 	public Boundary boundary;
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate = 0.5F;
 
+	private float nextFire = 0.0F;
 	private Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
+	}
+
+	void Update () {
+		if (Input.GetButton("Fire1") && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+		}
 	}
 	
 	// FixedUpdate is called once per frame after Update
