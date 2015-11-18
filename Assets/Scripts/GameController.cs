e313b733f2fc88db6d5148d728a2d13e98d026b6
Ishtyaq Habib <ishtyaq.habib@gmail.com>
2015-11-17 20:16:08 -0500

    Added Random Spawning of Hazards
    GameController spawns astroids at random locations off screen.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject hazard;
+	public Vector3 spawnValues;
+
+	void SpawnWaves () {
+		Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x,spawnValues.x),spawnValues.y,spawnValues.z);
+		Quaternion spawnRotation = Quaternion.identity;
+		Instantiate (hazard, spawnPosition, spawnRotation);
+	}
+
+	// Use this for initialization
+	void Start () {
+		SpawnWaves ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
