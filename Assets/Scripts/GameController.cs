feedcaff6e7e1d2547bab22fc1291adac1b0c9f5
Ishtyaq Habib <ishtyaq.habib@gmail.com>
2015-11-17 20:39:15 -0500

    Added Waves
    Created a wave co-routine with start time, spawn time, and wave time.
    Also, cleaned up explosion animation GameObject after hazard
    destruction.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,20 +5,27 @@ public class GameController : MonoBehaviour {
 
 	public GameObject hazard;
 	public Vector3 spawnValues;
-
-	void SpawnWaves () {
-		Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x,spawnValues.x),spawnValues.y,spawnValues.z);
-		Quaternion spawnRotation = Quaternion.identity;
-		Instantiate (hazard, spawnPosition, spawnRotation);
-	}
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
 
 	// Use this for initialization
 	void Start () {
-		SpawnWaves ();
+		StartCoroutine (SpawnWaves ());
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	IEnumerator SpawnWaves () {
+		yield return new WaitForSeconds (startWait);
+		while (true) {
+			for (int i = 0; i< hazardCount; i++) {
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x,spawnValues.x),spawnValues.y,spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds (waveWait);
+		}
 	}
+
 }
